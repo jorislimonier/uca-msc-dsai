b728972d9cde507d3d5c94497fbe5f564a23df3e
jorislimonier <joris.limonier@hotmail.fr>
2022-01-21 22:05:52 +0100

    big-data make correlation matrix, perform train-test split and convert time to datetime

--- big-data/final-project/final-project-LIMONIER.py
+++ big-data/final-project/final-project-LIMONIER.py
@@ -4,7 +4,8 @@ from pyspark.ml.feature import VectorAssembler, StringIndexer, OneHotEncoder
 from pyspark.ml import Pipeline
 from pyspark.ml.stat import Correlation
 from pyspark.sql.types import DoubleType
-from pyspark.sql.functions import isnan, when, count, col
+from pyspark.sql.functions import isnan, when, count, col, to_timestamp
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
 import pandas as pd
 import plotly.express as px
 
@@ -20,14 +21,19 @@ col_names = ["id", "time", "latitude", "longitude", "direction", "road", "traffi
 df = spark.read.option("delimiter", ";").csv("datexDataB40.csv")
 df = df.toDF(*col_names)
 df.show(2)
+
+# %%
 NUM_COL = ["avg_vehicle_speed", "vehicle_flow_rate", "traffic_concentration"]
 CAT_COLS = ["direction", "traffic_status"]
 
 # %% [markdown]
-# ## Exploratory Data Analysis
+# ## Exploratory Data Analysis (EDA)
+# We perform EDA on the whole data set, then we will perform a train-test split\
 # Show a summary of the data
 # %%
 df.summary().show()
+# %%
+df.printSchema()
 # %% [markdown]
 # Let's see how many unique values are in each column.
 # %%
@@ -35,28 +41,42 @@ for col_name in df.columns:
     unique_val = df.select(col_name).distinct().collect()
     print(f"--> {col_name}")
     print(f"\tunique values count: {len(unique_val)}")
-    if len(unique_val) <= 10000:
+    if len(unique_val) <= 1000:
         print(f"\tunique values: {[val[col_name] for val in unique_val]}")
 # %% [markdown]
-# Count the number of missing values
+# We look for missing values
 # %%
-# df.select([count(when(isnan(c), c)).alias(c) for c in df.columns]).show()
-df.select([count(when(isnan(c) | col(c).isNull() | (col(c) == "null") | (col(c) == "unknown"), c)).alias(c)
-          for c in df.columns]).show()
-# df.filter(df["avg_vehicle_speed"].isNull()).show()
-# %%
-df.filter((df["traffic_status"] == "unknown")).count()
-# %%
-distinct_avg_speed = df.select("avg_vehicle_speed").distinct()
-distinct_avg_speed.show(999)
-distinct_avg_speed.withColumn(
-    "avg_speed_double", distinct_avg_speed["avg_vehicle_speed"].cast("double")).show(999)
 
-# %%
-df == "null"
+
+def show_unknown_counts():
+    df.select([count(when(isnan(c) | col(c).isNull() | (col(c) == "null") | (col(c) == "unknown"), c)).alias(c)
+               for c in df.columns]).show()
+
+
+show_unknown_counts()
+
 # %% [markdown]
-# We notice that, even though there is no proper missing value, the `traffic_status` column contains some (string) "unkown" values. We will have to deal with that later. \
-# We also notice that the only road is "B40", so we can drop it.
+# We deal with missing values in the `traffic_status`.
+# %%
+df.groupby("traffic_status").count().show()
+print(f"Total number of rows in the data set: {df.count()}")
+# %% [markdown]
+# We see that there are 10309 "unkown" rows for the column `traffic_status`. This represents $\approx 5.8\%$ of the total number of rows. \
+# It is probably safe to drop these rows since the rest of the data is fairly clean, therefore we shouldn't have to drop more rows later on.
+# %%
+df = df.filter(df["traffic_status"] != "unknown")
+df.groupby("traffic_status").count().show()
+
+# %% [markdown]
+# We see that the `traffic_status` and `avg_vehicle_speed` have some unknown values
+# %%
+df = df.filter(df["traffic_status"] != "unknown")
+show_unknown_counts()
+# %% [markdown]
+# Actually, removing the "unknown" values from the `traffic_status` also removed the ones from `avg_vehicle_speed` (*i.e.* they were on the same rows)
+
+# %% [markdown]
+# We notice that the only road is "B40", so we can drop it.
 # %%
 
 
@@ -75,9 +95,17 @@ drop_if_exists("road")
 # The number of unique values for `id`, `latitude` and `longitude` is the same (10). \
 # We strongly suspect that for a given id, the latitude and longitude are always the same. Let's check it.
 # %%
-unique_ids = df.select("id").distinct().collect()
-unique_triples = df.select("id", "latitude", "longitude").distinct(
-).collect()  # unique triples (`id`, `latitude`, `longitude`)
+unique_ids = (
+    df.select("id")
+    .distinct()
+    .collect()
+)
+# unique triples (`id`, `latitude`, `longitude`)
+unique_triples = (
+    df.select("id", "latitude", "longitude")
+    .distinct()
+    .collect()
+)
 
 print(len(unique_triples) == len(unique_ids) == 10)
 
@@ -92,50 +120,56 @@ drop_if_exists("longitude")
 for num_col in NUM_COL:
     df = df.withColumn(num_col, df[num_col].cast("double"))
 
-df_num = df.select(NUM_COL).toPandas()
+# plot
+df_num = df.select(NUM_COL+["traffic_status"]).toPandas()
 for i in range(len(NUM_COL)):
     for j in range(i):
         col_x = NUM_COL[i]
         col_y = NUM_COL[j]
-        px.scatter(df_num, col_x, col_y).show()
+        px.scatter(data_frame=df_num, x=col_x, y=col_y,
+                   color="traffic_status").show()
 
 
 # %% [markdown]
 # There doesn't appear to be (much) correlation between the numerical columns.
-# Interestingly enough however, we noticed that the `traffic_concentration` column seems to be, if not categorical, at least fairly discrete.
+# Interestingly enough however, we noticed that the `traffic_concentration` column seems to be, if not categorical, at least fairly discrete.\
+# Additionally, we see that the `traffic_status` column is (probably) derived directly from the `avg_vehicle_speed`, so we will remove it for prediction, otherwise, it would be too easy to predict the `traffic_status` column.
 # %%
 df_corr = df.select(NUM_COL)
-df_corr.show(2)
-for num_col in NUM_COL:
-    df_corr = df_corr.withColumn(num_col, df_corr[num_col].cast("double"))
-print(df_corr.dtypes)
+# for num_col in NUM_COL:
+#     df_corr = df_corr.withColumn(num_col, df_corr[num_col].cast("double"))
 num_vector_col = "corr_features"
 corr_assembler = VectorAssembler(
     inputCols=df_corr.columns, outputCol=num_vector_col)
 df_vect = corr_assembler.transform(df_corr).select(num_vector_col)
-Correlation.corr(df_vect, num_vector_col)
-
+Correlation.corr(df_vect, num_vector_col).show(truncate=False)
 
 # %% [markdown]
-# Now that we studied the original data set sufficiently (performing only some redundancy removal), we can transform it.\
-# We will start by dealing with missing values in the `traffic_status` (counted as "unknown"), as mentioned previously. Let's see how many observations are concerned by this issue.
+# We see a strong correlation, but the columns studied are not linearly dependendent. We do not have hundreds of features so we decide not to drop any.\
+# Now we deal with the `time` column.
+
 # %%
-df.groupby("traffic_status").count().show()
-print(f"Total number of rows in the data: {df.count()}")
+time = df.select("time")
+time.show(2, truncate=False)
+df = df.withColumn("datetime", to_timestamp(df.time,))
+
+df.show(2)
+
+# %%
+from pyspark.sql.functions import year
+# df.withColumn("weekday", [date["datetime"].weekday() for date in df.select("datetime").collect()]).show(2)
+df.select(year("datetime")).show(2) # TODO: make columns for year, month, ...
 # %% [markdown]
-# We see that there are 10309 "unkown" rows for the column `traffic_status`. This represents $\approx 5.8\%$ of the total number of rows. \
-# It is probably safe to drop these rows since the rest of the data is fairly clean, therefore we shouldn't have to drop more rows.
+# ## Perform train-test split
 # %%
-df = df.filter(df["traffic_status"] != "unknown")
-df.groupby("traffic_status").count().show()
+train, test = df.randomSplit(weights=[.8, .2], seed=42)
+print(f"Number of observations in the train set: {train.count()}")
+print(f"Number of observations in the test set: {test.count()}")
+
 # %%
+CAT_COLS = ["direction", "traffic_status"]
 CAT_COLS_INDEXER = [f"{cat_col}_indexer" for cat_col in CAT_COLS]
 CAT_COLS_ONEHOT = [f"{cat_col}_vec" for cat_col in CAT_COLS]
-
-# for cat_col in CAT_COLS:
-#     traffic_status_indexer = StringIndexer(
-#         inputCol=cat_col, outputCol=f"{cat_col}_index")
-#     traffic_status_indexer.fit(df).transform(df).show(2)
 
 onehot_pipe = Pipeline(stages=[
     StringIndexer(inputCols=CAT_COLS, outputCols=CAT_COLS_INDEXER),
@@ -144,6 +178,12 @@ onehot_pipe = Pipeline(stages=[
     VectorAssembler(inputCols=CAT_COLS_ONEHOT, outputCol="cat_features"),
 ])
 
-onehot_pipe.fit(df).transform(df).show(2)
-df.select("traffic_status").distinct().collect()
-df.select("direction").distinct().collect()
+onehot_pipe.fit(train).transform(train).show(2)
+
+# %% [markdown]
+# Select numerical columns (disregarding `avg_vehicle_speed` as mentioned previously)
+# %%
+NUM_COL_PRED = ["vehicle_flow_rate", "traffic_concentration"]
+# %%
+
+# %%
