f24558f5b046e08b0508ee7faceae2c534d403c1
jorislimonier <joris.limonier@hotmail.fr>
2022-01-31 15:03:21 +0100

    intro-DL finish conditional random fields lab

--- misc/lateness/lateness.py
+++ misc/lateness/lateness.py
@@ -10,25 +10,56 @@ data.tail()
 
 
 # %%
-av_late = data.groupby("teacher").mean().sort_values(
-    by="start_time", ascending=False)
-px.bar(data_frame=av_late, y="start_time", color="start_time",
-       color_continuous_scale="Bluered", barmode="group", title="Mean lateness",)
+data["start_time"] = abs(data["start_time"])
+
+# %%
+av_late = data.groupby("teacher").mean().sort_values(by="start_time", ascending=False)
+px.bar(
+    data_frame=av_late,
+    y="start_time",
+    color="start_time",
+    color_continuous_scale="Bluered",
+    barmode="group",
+    title="Mean lateness",
+)
 
 
 # %%
-sum_late = data.groupby("teacher").sum().sort_values(
-    "start_time", ascending=False)
-px.bar(sum_late, y="start_time", color="start_time",
-       color_continuous_scale="Bluered", barmode="group", title="Cumulated lateness")
+sum_late = data.groupby("teacher").sum().sort_values("start_time", ascending=False)
+px.bar(
+    sum_late,
+    y="start_time",
+    color="start_time",
+    color_continuous_scale="Bluered",
+    barmode="group",
+    title="Cumulated lateness",
+)
 
 # %%
-median_late = data.groupby("teacher").median().sort_values(
-    "start_time", ascending=False)
-px.bar(av_late, y="start_time", color="start_time",
-       color_continuous_scale="Bluered", barmode="group", title="Median lateness")
+median_late = (
+    data.groupby("teacher").median().sort_values("start_time", ascending=False)
+)
+px.bar(
+    av_late,
+    y="start_time",
+    color="start_time",
+    color_continuous_scale="Bluered",
+    barmode="group",
+    title="Median lateness",
+)
 
 
 # %%
-px.bar(data.sort_values("start_time", ascending=False,), x="teacher", y="start_time",
-       color="start_time", color_continuous_scale="Bluered", barmode="group",)
+px.bar(
+    data.sort_values(
+        "start_time",
+        ascending=False,
+    ),
+    x="teacher",
+    y="start_time",
+    color="start_time",
+    color_continuous_scale="Bluered",
+    barmode="group",
+)
+
+# %%
