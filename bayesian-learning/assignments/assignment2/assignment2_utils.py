11097eaeffd75e5949e646f4c97125b943ce13b5
jorislimonier <joris.limonier@hotmail.fr>
2022-11-07 18:38:13 -0600

    bayesian-learning/ Complete assignment 2 exercise 1

--- bayesian-learning/assignments/assignment2/assignment2_utils.py
+++ bayesian-learning/assignments/assignment2/assignment2_utils.py
@@ -1,41 +1,80 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import seaborn as sns
 from scipy.optimize import minimize
 from scipy.special import logsumexp
 from scipy.stats import binom, multivariate_normal, norm, uniform
 from sklearn import datasets
+import plotly.io as pio
+
+pio.templates.default = "plotly_white"
 
 
 # Exercise 1
-def beta_binomial_distribution(n: int, y: int, alpha: int, beta: int) -> np.array:
+def beta_binom_distr(n: int, y: int, alpha: int, beta: int) -> np.array:
   """
-  Returns the beta-binomial distribution over the space of
-  parameter theta, given fixed parameters alpha, beta, y, and n.
+  Compute the beta binomial distribution as seen during lesson 2.
   """
-  # Declares a linear space for the parameter theta
-  theta_space = np.linspace(0, 1)
-  # Computes and return the distribution
-  distribution = theta_space ** (y + alpha - 1) * (1 - theta_space) ** (
-    n + beta - y - 1
-  )
-  return theta_space, distribution
+  # Declare theta values
+  theta_range = np.linspace(start=0, stop=1, num=100)
+  # Compute the distribution
+  distr = theta_range ** (y + alpha - 1) * (1 - theta_range) ** (n + beta - y - 1)
+  return theta_range, distr
 
 
-def beta_binomial_lapproximation(n: int, y: int, alpha: int, beta: int) -> np.array:
+def beta_binom_laplace_approx(n: int, y: int, alpha: int, beta: int) -> np.array:
   """
-  Returns the Laplace approximation of the beta binomial
-  distribution (i.e. Gaussian distribution) with parameters
-  derived from the original distribution's parameters alpha,
-  beta, y, and n.
+  Returns the Laplace approximation of the beta binomial distribution
+  as computed inexercise 1.
   """
   theta_max = (y + alpha - 1) / (n + alpha + beta - 2)
   hessian = -(y + alpha - 1) / (theta_max**2) - (n + beta - y - 1) / (
     (1 - theta_max) ** 2
   )
   variance = -1 / hessian
+
+  # Scipy takes the std as a second argument, not the variance
   return norm(theta_max, np.sqrt(variance))
+
+
+def plot_beta_binom_vs_laplace(y: int, n: int, alpha: int, beta: int) -> go.Figure:
+  """Plot the comparison between"""
+  # Compute the beta-binomial PDF
+  theta_values, beta_binomial = beta_binom_distr(n=n, y=y, alpha=alpha, beta=beta)
+
+  # Compute the Laplace approximation
+  laplace_approx = beta_binom_laplace_approx(n=n, y=y, alpha=alpha, beta=beta)
+
+  # Plot the results
+  fig = go.Figure()
+
+  # Add beta-binomial trace
+  beta_binomial_norm = beta_binomial / np.sum(beta_binomial)
+  approx_trace = go.Scatter(
+    x=theta_values,
+    y=beta_binomial_norm,
+    name="PDF of Beta-Binomial distribution",
+  )
+
+  # Add Laplace approximation trace
+  laplace_approx_norm = laplace_approx.pdf(theta_values) / np.sum(
+    laplace_approx.pdf(theta_values)
+  )
+  distr_trace = go.Scatter(
+    x=theta_values,
+    y=laplace_approx_norm,
+    name="Laplace approximation",
+  )
+  fig.add_traces(data=[approx_trace, distr_trace])
+  fig.update_layout(
+    showlegend=True,
+    title="Comparison ground truth vs Laplace approximation",
+  )
+
+  return fig
 
 
 # Exercise 2
